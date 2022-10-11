dc3ea04e7fe46a0008bc52c806edfbeb1f2b1bb2
Josetruck <olemiluis@gmail.com>
2022-10-11 09:00:01 +0200

    ejercicios de repaso

--- Prueba 7_10/ejercicio2/script.js
+++ Prueba 7_10/ejercicio2/script.js
@@ -5,3 +5,27 @@
 { "Nombre": "Pedro", "Apellido": "Jones"}
 ]
 Permite por medio de un formulario que el usuario pueda insertar empleados y cambiar su nombre, apellido o ambos. */
+
+var empleados = [
+    { Nombre: "John", Apellido: "Doe"},
+    { Nombre: "Anna", Apellido: "Smith"},
+    { Nombre: "Pedro", Apellido: "Jones"}]
+    
+for(let i=0;i<empleados.length;i++){
+    localStorage.setItem(`${empleados[i].Apellido}`,`${empleados[i].Nombre}`);
+}
+console.log(localStorage)
+
+function insertar(){
+    var nuevoNombre = document.getElementById("nombre").value;
+    var nuevoApellido = document.getElementById("apellido").value;
+    localStorage.setItem(`${nuevoApellido}`,`${nuevoNombre}` );
+    console.log(localStorage);
+}
+function modificar(){
+    var modifNombre = document.getElementById("nombre2").value;
+    var modifApellido = document.getElementById("apellido2").value;
+    localStorage.setItem(`${modifApellido}`,`${modifNombre}`);
+    console.log(localStorage)
+
+}
